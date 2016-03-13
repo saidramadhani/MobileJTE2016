16bac9d8c01a97b02ff36fb18a979d9b80ff86bb
Said Ramadhani <ramadhanisaid23@gmail.com>
2016-03-13 21:50:35 +0700

    Praktikum 18-Tebak Angka

---
+++ TebakAngka/app/src/main/java/com/example/acer/tebakangka/ActivityKedua.java
@@ -0,0 +1,23 @@
+package com.example.acer.tebakangka;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Created by Acer on 3/12/2016.
+ */
+public class ActivityKedua extends Activity {
+    @Override
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kedua);
+
+        Intent pesan = getIntent();
+        String pesanstr = pesan.getStringExtra("var1");
+
+        TextView txtLabel = (TextView) findViewById(R.id.textView2);
+        txtLabel.setText(pesanstr);
+    }
+}
---
+++ TebakAngka/app/src/main/java/com/example/acer/tebakangka/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.acer.tebakangka;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public void onClickbtnTebak (View view){
+        Random rand = new Random();
+        int n = rand.nextInt(100);
+
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String strEditText = editText.getText().toString();
+        Integer angkaEditText = Integer.valueOf(strEditText);
+
+
+
+
+        if (angkaEditText==n){
+            Intent pesan = new Intent(getApplicationContext(), ActivityKedua.class);
+            String hasil = Integer.toString(n);
+            pesan.putExtra("var1","Benar, angkanya adalah "+hasil);
+            startActivity(pesan);
+
+        }
+        else {
+            Intent pesan = new Intent(getApplicationContext(), ActivityKedua.class);
+            String hasil = Integer.toString(n);
+            pesan.putExtra("var1","Salah, angkanya adalah "+hasil);
+            startActivity(pesan);
+        }}}
